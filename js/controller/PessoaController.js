999cef8d0c9aa09ab7d6091729aff61ac3e484af
pedroPecly <pedrohpg23@hotmail.com>
2023-12-20 02:09:36 -0300

    no message

--- js/controller/PessoaController.js
+++ js/controller/PessoaController.js
@@ -1,7 +1,7 @@
-class PessoaController {
+class PessoaController{
     _pessoas = [];
 
-    inicializaElementos() {
+    inicializaElementos(){
         this.inpCodigo = document.querySelector("#inpCodigo");
         this.btnSalvar = document.querySelector("#btnSalvar");
         this.bodyPessoa = document.querySelector("#bodyPessoa");
@@ -11,117 +11,116 @@ class PessoaController {
         this.divTotal = document.querySelector("#divTotal");
     }
 
-    alterar(pId){
+    excluir(pId){
         if(pId){
-            if(confirm("deseja excluir")){
-                this._pessoas = this._pessoas.filter(p => p.id != pId);
-            }
-        }
-
-        this.exibir
-    }
-
-    salvar(pId, pNome, pSobrenome, pSalario) {
-        let p;
-
-        if (pId == 0) {
-            p = new Pessoa();
-        } else {
-            p = this._pessoas.filter(p => p.id == pId)[0];
-            if (!p) {
-                p = new Pessoa();
-            }
-        }
-
-        p.nome = pNome;
-        p.sobrenome = pSobrenome;
-        p.salario = pSalario;
-
-        if (pId == 0) {
-            p.id = this._pessoas.length + 1;
-            this._pessoas.push(p);
-        }
-    }
-
-    excluir(pId) {
-        if (pId) {
-            if (confirm("deseja excluir ?")) {
+            if (confirm('Deseja excluir?')){
                 this._pessoas = this._pessoas.filter(p => p.id != pId);
             }
         }
         this.exibir();
     }
 
-    exibir() {
-        let linhas = "";
-        this._pessoas.forEach(p => {
-            linhas +=
-                `
-                <tr>
-                    <td>${p.id}</td>
-                    <td>${p.nome}</td>
-                    <td>${p.sobrenome}</td>
-                    <td>${p.salario}</td>
-                    <td>${p.nomeCompleto()}</td>
-                    <td>
-                        <button id="btnAlterar${p.id}" class="btn btn-primary btn-db" data-id="${p.id}">Alterar</button>
-                        <button id="btnExcluir${p.id}" class="btn btn-danger btn-db" data-id="${p.id}">Excluir</button>
-                    </td>
-                </tr>
-            `
-        });
+    alterar(pId){
+        if(pId){
+            let p = this._pessoas.filter(p => p.id == pId)[0];
 
-        this.bodyPessoa.innerHTML = linhas;
-        this.eventosBD();
-        this.calcularTotal();
+            if(p){
+                this.inpCodigo.value = p.id;
+                this.inpNome.value = p.nome;
+                this.inpSobrenome.value = p.sobrenome;
+                this.inpSalario.value = p.salario;
+            }
+        }
     }
 
-    eventosBD() {
+    salvar(pId, pNome, pSobrenome, pSalario){
+        let p;
+        if(pId == 0){
+            p = new Pessoa();
+        } else {
+          p = this._pessoas.filter(p => p.id == pId)[0];
+          if(!p)
+            p = new Pessoa();
+        }
+        p.nome = pNome;
+        p.sobrenome = pSobrenome;
+        p.salario = pSalario;
+
+        if(pId == 0){
+            p.id = this._pessoas.length + 1;
+            this._pessoas.push(p);
+        }
+
+    }
+
+    exibir(){
+        let linhas = "";
+        this._pessoas.forEach(p => {
+            linhas += 
+            `<tr>
+               <td>${p.id}</td>
+               <td>${p.nome}</td>
+               <td>${p.sobrenome}</td>
+               <td>${p.salario}</td>
+               <td>${p.NomeCompleto()}</td>
+               <td>
+                 <button id="btnAlterar${p.id}" class="btn btn-primary btn-db" data-id="${p.id}">Alterar</button>
+                 <button id="btnExcluir${p.id}" class="btn btn-danger btn-db" data-id="${p.id}">Excluir</button>
+               </td>
+             </tr>                     
+            `
+        });
+        this.bodyPessoa.innerHTML = linhas;
+        this.eventosDB();
+        this.calculaTotal();
+    }
+
+    eventosDB(){
         let self = this;
         this.bodyPessoa.querySelectorAll('.btn-db').forEach(btn => {
             let id = btn.id.toUpperCase();
-
-            if (id.indexOf('BTNEXCLUIR') != -1) {
+                       
+            if (id.indexOf('BTNEXCLUIR') != -1){
                 btn.onclick = () => {
                     self.excluir(btn.dataset.id);
                 }
             }
 
-            if (id.indexOf('BTNALTERAR') != -1) {
+            if (id.indexOf('BTNALTERAR') != -1){
                 btn.onclick = () => {
-                    alert('alterar');
+                    self.alterar(btn.dataset.id);
                 }
-            }
+            }          
         });
     }
 
-    calcularTotal() {
-        let total = this._pessoas.reduce(function (pTotal , p){
+    calculaTotal(){
+        let total = this._pessoas.reduce((pTotal, p) =>{
             return pTotal + parseFloat(p.salario);
-        }, 0);
-        this.divTotal.innerHTML = "R$" + total;
+        },0);
+        this.divTotal.innerHTML = `R$ ${total}`;
     }
 
-    limpar() {
+    limpar(){
         this.inpNome.value = "";
-        this.inpSalario = 0;
-        this.inpSobrenome = "";
+        this.inpSalario.value = 0;
+        this.inpSobrenome.value = "";
+        this.inpCodigo.value = 0;
     }
 
-    inicializaEventos() {
-        this.btnSalvar.addEventListener("click", function () {
-            if(this.inpNome || this.inpSobrenome || this.inpSalario){
-                this.salvar(this.inpCodigo.value, this.inpNome.value, this.inpSobrenome.value, this.inpSalario.value);
-                this.exibir();
-                this.limpar();
-                this.inpNome.focus();
-            } else {
-                alert("campo nao preenchido");
-            }
-        });
+    inicializaEventos(){
+        this.btnSalvar.onclick = () => {
+            this.salvar(this.inpCodigo.value,this.inpNome.value,this.inpSobrenome.value,
+                this.inpSalario.value);
+            
+            this.exibir();
+            this.limpar();
+            this.inpNome.focus();
+
+        }
     }
 
-    constructor() {
+    constructor(){
         this.inicializaElementos();
         this.inicializaEventos();
     }
